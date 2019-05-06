81b5f1a65ef5c3deecf8e7013af4179b5e293f6b
Ryan Kretch <rkretch91@gmail.com>
2019-05-06 19:41:43 +0200

    banner working

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = props => (
+  <div className="banner" style={{backgroundImage: 'url(\'https://res.cloudinary.com/dofuzeof4/image/upload/v1557163617/unnamed_bli2tp.jpg\')'}}>
+    <span className="slogan">REDEFINING PUBLIC RESTROOMS</span>
+  </div>
+)
+
+export default Banner;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,7 +18,7 @@ const Layout = ({ children, location, i18nMessages }) => {
               languages {
                 defaultLangKey
                 langs
-              }      
+              }
             }
           }
         }
@@ -46,8 +46,6 @@ const Layout = ({ children, location, i18nMessages }) => {
               <div
                 style={{
                   margin: '0 auto',
-                  maxWidth: 960,
-                  padding: '0px 1.0875rem 1.45rem',
                   paddingTop: 0,
                 }}
               >
