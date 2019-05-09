6f0d897dc2d6780d7dc5b28d9e80acad51fdeeb7
Ryan Kretch <rkretch91@gmail.com>
2019-05-09 11:59:29 +0200

    minor changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = props => (
   <div className="banner" style={{backgroundImage: 'url(\'https://res.cloudinary.com/dofuzeof4/image/upload/v1557163617/unnamed_bli2tp.jpg\')'}}>
-    <span className="slogan">REDEFINING PUBLIC RESTROOMS</span>
+    <span className="slogan">{props.slogan}</span>
   </div>
 )
 
