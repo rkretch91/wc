b5969b0fa1eec8a023c40a14d4cbd476511182cc
Ryan Kretch <rkretch91@gmail.com>
2019-08-03 17:21:14 +0200

    testing again

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Div100vh from 'react-div-100vh';
 
 const Banner = props => (
-  <div className="banner foo">
-    <span className="slogan">{props.slogan}</span>
-  </div>
+  <Div100vh>
+    <div className="banner foo">
+      <span className="slogan">{props.slogan}</span>
+    </div>
+  </Div100vh>
 )
 
 export default Banner;
